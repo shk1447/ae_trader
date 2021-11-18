82646b52c12f9a4f0603afb0fd753946f89bce4c
seokhwankim <seokhwan.kim@makinarocks.ai>
2021-11-18 14:40:39 +0900

    add bollinger bands

--- server/routes/api/stock/index.js
+++ server/routes/api/stock/index.js
@@ -1,15 +1,12 @@
 const collector = require('./modules/NaverFinance');
 const analysis = require('./modules/Analysis');
 const connector = require('../../../connector');
-const SMA = require('technicalindicators').SMA
-const BB = require('technicalindicators').BollingerBands;
-const CLOUD = require('technicalindicators').IchimokuCloud
 
 const cliProgress = require('cli-progress');
 
 const dfd = require("danfojs-node");
 const { segmentation } = require('./modules/Analysis');
-const { IchimokuCloud } = require('technicalindicators');
+const { IchimokuCloud, BollingerBands, SMA } = require('technicalindicators');
 
 module.exports = {
   get: {
@@ -81,131 +78,142 @@ module.exports = {
           let rows = [];
           let recommended_rows = []
           const data = await collector.getSise(item.stock_code, days);
-          
-          const origin_data = await connector.dao.StockData.getTable().where('date', '<', data[0].date);
-          
-          const all_data = origin_data.concat(data);
-          let prev_result;
-          for (let i = all_data.length - data.length; i < all_data.length; i++) {
-            let row = all_data[i];
-            row['code'] = item.stock_code;
-            try {
-              var day_power = (row.close - row.open);
-              var down_power = (row.high - Math.min(row.open, row.close));
-              var up_power = (Math.max(row.open, row.close) - row.low);
-              var power = (up_power - down_power + day_power) * (row.volume / item.stock_total);
+          if(data.length > 0) {
+            const origin_data = await connector.dao.StockData.getTable().where('date', '<', data[0].date);
+            const all_data = origin_data.concat(data);
 
-              let result = {
-                curr_trend: 0,
-                init_trend: 0,
-                segmentation: [],
-                upward_point: [],
-                downward_point: [],
-              };
-              analysis.segmentation([...all_data].splice(0, i + 1), result);
-              let insight = analysis.cross_point(result, row);
-              result['insight'] = insight
-              metadata.push(result);
-              const period = 5;
-              
-              result.segmentation.sort((a, b) => a.from.date - b.from.date)
+            let prev_result;
+            for (let i = all_data.length - data.length; i < all_data.length; i++) {
+              let row = all_data[i];
+              row['code'] = item.stock_code;
+              try {
+                var day_power = (row.close - row.open);
+                var down_power = (row.high - Math.min(row.open, row.close));
+                var up_power = (Math.max(row.open, row.close) - row.low);
+                var power = (up_power - down_power + day_power) * (row.volume / item.stock_total);
 
-              let clouds = new IchimokuCloud({
-                high: [...all_data].splice(0, i + 1).map((d) => d.high),
-                low: [...all_data].splice(0, i + 1).map((d) => d.low),
-                conversionPeriod: 9,
-                basePeriod: 26,
-                spanPeriod: 52,
-                displacement: 26
-              }).result;
-    
-              let cloud_data = new Array(77).concat(clouds)
+                let result = {
+                  curr_trend: 0,
+                  init_trend: 0,
+                  segmentation: [],
+                  upward_point: [],
+                  downward_point: [],
+                };
+                
+                analysis.segmentation([...all_data].splice(0, i + 1), result, 'close');
+                let insight = analysis.cross_point(result, row, 'close');
+                result['insight'] = insight
+                metadata.push(result);
+                const period = 5;
+                
+                result.segmentation.sort((a, b) => a.from.date - b.from.date)
 
-              if (metadata.length >= period) {
-                const short_mean_arr = metadata.slice(i - (period - 1), i + 1);
+                let clouds = new IchimokuCloud({
+                  high: [...all_data].splice(0, i + 1).map((d) => d.high),
+                  low: [...all_data].splice(0, i + 1).map((d) => d.low),
+                  conversionPeriod: 9,
+                  basePeriod: 26,
+                  spanPeriod: 52,
+                  displacement: 26
+                }).result;
 
-                const support_arr = short_mean_arr.map((d) => d.insight.support);
-                const resist_arr = short_mean_arr.map((d) => d.insight.resist);
-                const future_support_arr = short_mean_arr.map((d) => d.insight.future_support);
-                const future_resist_arr = short_mean_arr.map((d) => d.insight.future_resist);
-                insight.support = _.mean(support_arr);
-                insight.resist = _.mean(resist_arr);
-                insight.future_support = _.mean(future_support_arr);
-                insight.future_resist = _.mean(future_resist_arr);
+                let bands = new BollingerBands({
+                  period:20,
+                  stdDev:2,
+                  values:[...all_data].splice(0, i + 1).map((d) => d.close),
+                }).result
+      
+                let band_data = new Array(19).concat(bands)
+                let cloud_data = new Array(77).concat(clouds)
 
-                if (prev_result) {
-                  let meta = {
-                    curr_trend: result.curr_trend,
-                    init_trend: result.init_trend,
-                    segmentation: result.segmentation.length,
-                    upward_point: result.upward_point.length,
-                    downward_point: result.downward_point.length,
-                    insight: insight,
-                    power: power
-                  }
+                if (metadata.length >= period) {
+                  const short_mean_arr = metadata.slice(i - (period - 1), i + 1);
 
-                  /*
-                    spanA > spanB 양운
-                    spanA < spanB 음운
-                    conversion 전환선
-                    base 기준선
-                  */
-                  meta['cloud'] = cloud_data[cloud_data.length - 27]
+                  const support_arr = short_mean_arr.map((d) => d.insight.support);
+                  const resist_arr = short_mean_arr.map((d) => d.insight.resist);
+                  const future_support_arr = short_mean_arr.map((d) => d.insight.future_support);
+                  const future_resist_arr = short_mean_arr.map((d) => d.insight.future_resist);
+                  insight.support = _.mean(support_arr);
+                  insight.resist = _.mean(resist_arr);
+                  insight.future_support = _.mean(future_support_arr);
+                  insight.future_resist = _.mean(future_resist_arr);
 
-                  // 미래 구름
-                  let future_conversion = 0;
-                  let future_trend = 0;
-                  let train = [];
-                  for(var c = 26; c > 0; c--) {
-                    const future_cloud = cloud_data[cloud_data.length - c];
-                    if(future_cloud) {
-                      future_trend += (future_cloud.spanA - future_cloud.spanB)
-                      future_conversion += future_cloud.conversion;
-                      train.push((future_cloud.spanA - future_cloud.spanB))
+                  if (prev_result) {
+                    let meta = {
+                      curr_trend: result.curr_trend,
+                      init_trend: result.init_trend,
+                      segmentation: result.segmentation.length,
+                      upward_point: result.upward_point.length,
+                      downward_point: result.downward_point.length,
+                      insight: insight,
+                      power: power
                     }
-                  }
-                  meta['future_trend'] = future_trend / 26;
-                  meta['future_conversion'] = future_conversion / 26;
-                  meta['train'] = train;
 
-                  row['meta'] = JSON.stringify(meta);
+                    /*
+                      spanA > spanB 양운
+                      spanA < spanB 음운
+                      conversion 전환선
+                      base 기준선
+                    */
+                    meta['cloud'] = cloud_data[cloud_data.length - 27]
+                    meta['band'] = band_data[band_data.length - 1]
 
-                  if (insight.support > insight.resist && prev_result.insight.support < prev_result.insight.resist && prev_result.insight.resist > insight.resist && prev_result.insight.support < insight.support && insight.resist + insight.future_support < insight.future_resist + insight.support && prev_result.insight.resist + prev_result.insight.future_support > prev_result.insight.future_resist + prev_result.insight.support) {
-                    row['marker'] = '매수';
-                    let futures = all_data.slice(i + 1, i + 61);
-                    if (futures.length == 60) {
-                      var max_point = [...futures].sort((a, b) => b.high - a.high)[0];
-                      var high_rate = max_point.high / row.close * 100;
-                      row['result'] = high_rate;
+                    // 미래 구름
+                    let future_conversion = 0;
+                    let future_trend = 0;
+                    let train = [];
+                    for(var c = 26; c > 0; c--) {
+                      const future_cloud = cloud_data[cloud_data.length - c];
+                      if(future_cloud) {
+                        future_trend += (future_cloud.spanA - future_cloud.spanB)
+                        future_conversion += future_cloud.conversion;
+                        train.push(future_cloud)
+                      }
                     }
+                    meta['future_trend'] = future_trend / 26;
+                    meta['future_conversion'] = future_conversion / 26;
+                    meta['train'] = train;
 
                     row['meta'] = JSON.stringify(meta);
-                    recommended_rows.push(row)
+
+                    if (insight.support > insight.resist && prev_result.insight.support < prev_result.insight.resist && prev_result.insight.resist > insight.resist && prev_result.insight.support < insight.support && insight.resist + insight.future_support < insight.future_resist + insight.support && prev_result.insight.resist + prev_result.insight.future_support > prev_result.insight.future_resist + prev_result.insight.support) {
+                      row['marker'] = '매수';
+                      let futures = all_data.slice(i + 1, i + 61);
+                      if (futures.length == 60) {
+                        var max_point = [...futures].sort((a, b) => b.high - a.high)[0];
+                        var high_rate = max_point.high / row.close * 100;
+                        row['result'] = high_rate;
+                      }
+
+                      row['meta'] = JSON.stringify(meta);
+                      recommended_rows.push(row)
+                      
+                    }
                   }
+                  prev_result = result;
                 }
-                prev_result = result;
+              } catch (error) {
+                console.log(error)
               }
-            } catch (error) {
-              console.log(error)
+
+              rows.push(row);
             }
 
-            rows.push(row);
-          }
-
-          if (rows.length > 0) {
-            if (rows.length > 100) {
-              await connector.dao.StockData.batchInsert(rows);
-            } else {
-              await connector.dao.StockData.insert(rows).onConflict(['code', 'date']).merge();
+            if (rows.length > 0) {
+              if (rows.length > 100) {
+                await connector.dao.StockData.batchInsert(rows);
+              } else {
+                await connector.dao.StockData.insert(rows).onConflict(['code', 'date']).merge();
+              }
             }
-          }
 
-          if (recommended_rows.length > 0) {
-            connector.dao.StockData.table_name = "stock_data";
-            if (recommended_rows.length > 100) {
-              await connector.dao.StockData.batchInsert(recommended_rows);
-            } else {
-              await connector.dao.StockData.insert(recommended_rows).onConflict(['code', 'date']).merge();
+            if (recommended_rows.length > 0) {
+              connector.dao.StockData.table_name = "stock_data";
+              if (recommended_rows.length > 100) {
+                await connector.dao.StockData.batchInsert(recommended_rows);
+              } else {
+                await connector.dao.StockData.insert(recommended_rows).onConflict(['code', 'date']).merge();
+              }
             }
           }
 
--- server/routes/api/stock/modules/Analysis.js
+++ server/routes/api/stock/modules/Analysis.js
@@ -1,14 +1,14 @@
 const moment = require('moment');
 // slope(기울기) : y이동량 / x이동량
-function getLineIntersect(point1, slope1, point2, slope2) {
+function getLineIntersect(point1, slope1, point2, slope2, key) {
   var intersectX = 0,
     intersectY = 0;
 
   // x = (m1x1 - m2x2 + y2 - y1) / (m1 - m2)
-  intersectX = ((slope1 * point1.date) - (slope2 * point2.date) + point2.close - point1.close)
+  intersectX = ((slope1 * point1.date) - (slope2 * point2.date) + point2[key] - point1[key])
     / (slope1 - slope2);
   // y = m1(x - x1) + y1
-  intersectY = slope1 * (intersectX - point1.date) + point1.close;
+  intersectY = slope1 * (intersectX - point1.date) + point1[key];
 
   var result = {
     date: intersectX,
@@ -19,12 +19,12 @@ function getLineIntersect(point1, slope1, point2, slope2) {
   return result;
 }
 
-function trend(data, result, trend_type, firstValue) {
+function trend(data, result, trend_type, firstValue, key) {
   if (data.length > 1) {
     var start_x = data[0].date / 1000000;
-    var start_y = data[0].close;
+    var start_y = data[0][key];
     var end_x = data[data.length - 1].date / 1000000;
-    var end_y = data[data.length - 1].close;
+    var end_y = data[data.length - 1][key];
     var std_degree = Math.atan2(Math.abs(end_y - start_y), Math.abs(end_x - start_x)) * 180 / Math.PI;
 
     var min_idx = null;
@@ -36,7 +36,7 @@ function trend(data, result, trend_type, firstValue) {
     data.forEach((d, i) => {
 
       var dynamic_x = d.date / 1000000;
-      var dynamic_y = d.close;
+      var dynamic_y = d[key];
       var dynamic_degree = Math.atan2(Math.abs(dynamic_y - start_y), Math.abs(dynamic_x - start_x)) * 180 / Math.PI;
       _volume += d.volume;
       _cnt++;
@@ -60,19 +60,19 @@ function trend(data, result, trend_type, firstValue) {
     if (min_idx) {
       result[trend_type + '_point'].push({ avg_volume: _volume / _cnt, degree: min_degree, high: data[min_idx].high, low: data[min_idx].low, close: _close, diff: diff, date: data[min_idx].date, seg_idx: result.segmentation.length })
       // result.curr_point.push({ avg_volume: _volume / _cnt, degree: min_degree, high: _high, low: _low, close: _close, diff: diff, date: moment(data[min_idx].date).format('YYYY-MM-DD') })
-      trend(data.slice(min_idx, data.length), result, trend_type, data[min_idx].close)
+      trend(data.slice(min_idx, data.length), result, trend_type, data[min_idx][key], key)
     }
   }
 }
 
-function segmentation(data, result) {
+function segmentation(data, result, key) {
   if (data.length > 1) {
     const max = data.reduce(function (prev, curr) {
-      return prev.close > curr.close ? prev : curr
+      return prev[key] > curr[key] ? prev : curr
     })
 
     const min = data.reduce(function (prev, curr) {
-      return prev.close < curr.close ? prev : curr
+      return prev[key] < curr[key] ? prev : curr
     })
 
     const trend_type = max.date > min.date ? 'upward' : 'downward';
@@ -82,7 +82,7 @@ function segmentation(data, result) {
 
     switch (trend_type) {
       case 'upward':
-        trend([...data].slice(min_idx, max_idx), result, trend_type, min.close);
+        trend([...data].slice(min_idx, max_idx), result, trend_type, min[key], key);
         data = data.slice(max_idx)
         result.curr_trend = trend_type == 'upward' ? 1 : -1
         result.segmentation.push({
@@ -91,7 +91,7 @@ function segmentation(data, result) {
 
         break;
       case 'downward':
-        trend([...data].slice(max_idx, min_idx), result, trend_type, max.close);
+        trend([...data].slice(max_idx, min_idx), result, trend_type, max[key], key);
         data = data.slice(min_idx)
         result.curr_trend = trend_type == 'upward' ? 1 : -1;
         result.segmentation.push({
@@ -101,37 +101,37 @@ function segmentation(data, result) {
         break;
     }
 
-    segmentation(data, result);
+    segmentation(data, result, key);
   }
 }
 
-function cross_point(result, pick) {
+function cross_point(result, pick, key) {
   let cross = []
   result.upward_point.forEach((up, up_idx) => {
     result.downward_point.forEach((down, down_idx) => {
       if (up.seg_idx > 0 && down.seg_idx > 0) {
-        var test = getLineIntersect(up, up.diff / 86400000, down, down.diff / 86400000);
+        var test = getLineIntersect(up, up.diff / 86400000, down, down.diff / 86400000, key);
         var std = up.date > down.date ? up : down;
-        if (test.close > result.segmentation[std.seg_idx].min.low && result.segmentation[std.seg_idx].max.high > test.close) {
+        if (test[key] > result.segmentation[std.seg_idx].min.low && result.segmentation[std.seg_idx].max.high > test[key]) {
           cross.push(test);
         }
       }
     })
   });
   var resist = cross.filter((d) => {
-    return d.close >= pick.close && moment(pick.date) > moment(d.date)
+    return d[key] >= pick[key] && moment(pick.date) > moment(d.date)
   })
 
   var support = cross.filter((d) => {
-    return d.close <= pick.close && moment(pick.date) > moment(d.date)
+    return d[key] <= pick[key] && moment(pick.date) > moment(d.date)
   })
 
   var future_resist = cross.filter((d) => {
-    return d.close >= pick.close && moment(pick.date) <= moment(d.date)
+    return d[key] >= pick[key] && moment(pick.date) <= moment(d.date)
   })
 
   var future_support = cross.filter((d) => {
-    return d.close <= pick.close && moment(pick.date) <= moment(d.date)
+    return d[key] <= pick[key] && moment(pick.date) <= moment(d.date)
   })
 
   return {
