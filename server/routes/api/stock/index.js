4126996c5dcc49f34ce259d4238575c51f79d020
shk1447 <sukan8822@gmail.com>
2021-11-19 22:53:33 +0900

    bug fix - 불필요한 데이터 수집안하도록 수정 (거래정지된 기간의 데이터) - 지표 정보 및 조건 수정

--- server/routes/api/stock/index.js
+++ server/routes/api/stock/index.js
@@ -13,7 +13,7 @@ let collecting = false;
 module.exports = {
   get: {
     "initialize": async (req, res, next) => {
-      if(!collecting) {
+      if (!collecting) {
         let stockList = await collector.getStockList();
 
         stockList = stockList.filter((d) => (!(d.stock_name.includes("KODEX") || d.stock_name.includes("TIGER") || d.stock_name.includes("KOSEF") || d.stock_name.includes("HANARO") || d.stock_name.includes("KINDEX") || d.stock_name.includes("선물") || d.stock_name.includes("인버스") || d.stock_name.includes("KBSTAR") || d.stock_name.includes("ARIRANG") || d.stock_name.includes("ETN") || d.stock_name.includes("고배당"))))
@@ -43,7 +43,7 @@ module.exports = {
       res.status(200).send('OK');
     },
     "clear": async (req, res, next) => {
-      if(!collecting) {
+      if (!collecting) {
         const stockList = await connector.dao.StockList.select({});
 
         connector.dao.StockData.table_name = "stock_data";
@@ -69,7 +69,7 @@ module.exports = {
       res.status(200).send('OK');
     },
     "collect": async (req, res, next) => {
-      if(!collecting) {
+      if (!collecting) {
         collecting = true;
         const code = req.query.code ? { stock_code: req.query.code } : {};
         const days = req.query.days ? parseInt(req.query.days) : 5;
@@ -82,11 +82,11 @@ module.exports = {
           if (step < stockList.length) {
             var item = stockList[step];
             connector.dao.StockData.table_name = "stock_data_" + item.stock_code;
-            let metadata = [];
+
             let rows = [];
             let recommended_rows = []
             const data = await collector.getSise(item.stock_code, days);
-            if(data.length > 0) {
+            if (data.length > 0) {
               const origin_data = await connector.dao.StockData.getTable().where('date', '<', data[0].date);
               const all_data = origin_data.concat(data);
 
@@ -117,17 +117,14 @@ module.exports = {
                   // analysis.segmentation([...all_data].splice(0, i + 1), result_vol, 'volume');
                   analysis.segmentation([...all_data].splice(0, i + 1), result, 'close');
                   let insight = analysis.cross_point(result, row, 'close');
-                  result['insight'] = insight
-                  metadata.push(result);
-                  const period = 5;
-                  
+
                   result.segmentation.sort((a, b) => a.from.date - b.from.date)
                   result.upward_point.sort((a, b) => a.date - b.date)
                   result.downward_point.sort((a, b) => a.date - b.date)
 
                   let clouds = new IchimokuCloud({
-                    high: [...all_data].splice(0, i + 1).map((d) => d.high),
-                    low: [...all_data].splice(0, i + 1).map((d) => d.low),
+                    high: [...all_data].slice(i - 78, i).map((d) => d.high),
+                    low: [...all_data].slice(i - 78, i).map((d) => d.low),
                     conversionPeriod: 9,
                     basePeriod: 26,
                     spanPeriod: 52,
@@ -135,116 +132,88 @@ module.exports = {
                   }).result;
 
                   let bands = new BollingerBands({
-                    period:20,
-                    stdDev:2,
-                    values:[...all_data].splice(0, i + 1).map((d) => d.close),
+                    period: 20,
+                    stdDev: 2,
+                    values: [...all_data].slice(i - 20, i).map((d) => d.close),
                   }).result
 
-                  let obv = new OBV({
-                    close:[...all_data].splice(0, i + 1).map((d) => d.close),
-                    volume:[...all_data].splice(0, i + 1).map((d) => d.volume)
-                  }).result;
-                  
-        
-                  let band_data = new Array(19).concat(bands)
-                  let cloud_data = new Array(77).concat(clouds)
-                  let obv_data = new Array(1).concat(obv)
+                  let band_data = bands
+                  let cloud_data = clouds;
+                  let meta = {
+                    curr_trend: result.curr_trend,
+                    init_trend: result.init_trend,
+                    segmentation: result.segmentation.length,
+                    upward_point: result.upward_point.length,
+                    downward_point: result.downward_point.length,
+                    insight: insight,
+                    power: power
+                  };
+                  meta['cloud'] = cloud_data[0];
+                  meta['band'] = band_data[0];
 
-                  if (metadata.length >= period) {
-                    const short_mean_arr = metadata.slice(i - (period - 1), i + 1);
+                  /*
+                    spanA > spanB 양운
+                    spanA < spanB 음운
+                    conversion 전환선
+                    base 기준선
+                  */
 
-                    const support_arr = short_mean_arr.map((d) => d.insight.support);
-                    const resist_arr = short_mean_arr.map((d) => d.insight.resist);
-                    insight.support = _.mean(support_arr);
-                    insight.resist = _.mean(resist_arr);
-
-                    const future_support_arr = short_mean_arr.map((d) => d.insight.future_support);
-                    const future_resist_arr = short_mean_arr.map((d) => d.insight.future_resist);
-                    insight.future_support = _.mean(future_support_arr);
-                    insight.future_resist = _.mean(future_resist_arr);
-
-                    if (prev_result) {
-                      let meta = {
-                        curr_trend: result.curr_trend,
-                        init_trend: result.init_trend,
-                        segmentation: result.segmentation.length,
-                        upward_point: result.upward_point.length,
-                        downward_point: result.downward_point.length,
-                        insight: insight,
-                        power: power
-                      }
-
-                      /*
-                        spanA > spanB 양운
-                        spanA < spanB 음운
-                        conversion 전환선
-                        base 기준선
-                      */
-                      meta['cloud'] = cloud_data[cloud_data.length - 27]
-                      meta['band'] = band_data[band_data.length - 1]
-                      meta['obv'] = obv_data[obv_data.length - 1];
-
-                      // 미래 구름
-                      let future_conversion = 0;
-                      let future_base = 0;
-                      let future_trend = 0;
-                      let obv_change = 0;
-                      let prev_past_obv;
-                      for(var c = 26; c > 0; c--) {
-                        const past_obv = obv_data[obv_data.length - c];
-                        if(prev_past_obv) {
-                          obv_change += (past_obv - prev_past_obv)
-                        }
-                        const future_cloud = cloud_data[cloud_data.length - c];
-                        if(future_cloud) {
-                          future_trend += (future_cloud.spanA - future_cloud.spanB)
-                          future_conversion += future_cloud.conversion;
-                          future_base += future_cloud.base;
-                        }
-                        prev_past_obv = past_obv;
-                      }
-                      meta['future_trend'] = future_trend / 26;
-                      meta['future_conversion'] = future_conversion / 26;
-                      meta['future_base'] = future_base / 26;
-                      meta['obv_change'] = obv_change / 26;
-
-                      row['meta'] = JSON.stringify(meta);
-
-                      if (insight.support > insight.resist && prev_result.insight.support < prev_result.insight.resist && meta.future_base > meta.cloud.base && prev_result.insight.resist > insight.resist && prev_result.insight.support < insight.support && prev_result.insight.support + prev_result.insight.future_resist <= prev_result.insight.resist + prev_result.insight.future_support && insight.support + insight.future_resist >= insight.future_support + insight.resist) {
-                        row['marker'] = '매수';
-                        let futures = all_data.slice(i + 1, i + 61);
-                        if (futures.length == 60) {
-                          var buy;
-                          var sell;
-                          [...futures].forEach((d) => {
-                            if(!buy && d.low > meta.future_base && d.close > meta.future_base) {
-                              buy = d.close
-                            }
-                            if(buy && d.high > buy*1.05) {
-                              sell = d.high
-                            }
-                          })
-                          // var max_point = [...futures].sort((a, b) => b.high - a.high)[0];
-                          // var high_rate = max_point.high / row.close * 100;
-                          if(sell && buy) {
-                            row['result'] = sell/buy*100;
-                          }
-
-                          if(buy && !sell) {
-                            row['result'] = "매도 실패"
-                          }
-
-                          if(!buy) {
-                            row['result'] = "매수 실패"
-                          }
-                        }
-
-                        row['meta'] = JSON.stringify(meta);
-                        recommended_rows.push(row)
-                      }
+                  // 미래 구름
+                  let future_conversion = 0;
+                  let future_base = 0;
+                  let future_trend = 0;
+                  for (var c = 1; c < cloud_data.length; c++) {
+                    const future_cloud = cloud_data[c];
+                    if (future_cloud) {
+                      future_trend += (future_cloud.spanA - future_cloud.spanB)
+                      future_conversion += future_cloud.conversion;
+                      future_base += future_cloud.base;
                     }
-                    prev_result = result;
                   }
+                  meta['future_trend'] = future_trend / 26;
+                  meta['future_conversion'] = future_conversion / 26;
+                  meta['future_base'] = future_base / 26;
+
+                  result['meta'] = meta;
+
+                  row['meta'] = JSON.stringify(meta);
+
+                  if (prev_result) {
+                    if (prev_result.meta.insight.support + prev_result.meta.insight.future_resist <= prev_result.meta.insight.resist + prev_result.meta.insight.future_support && insight.support + insight.future_resist > insight.future_support + insight.resist && !((meta.cloud.spanA - meta.cloud.spanB) < 0 && meta.cloud.spanB > row.close) && meta.band.lower < row.close) {
+                      row['marker'] = '매수';
+                      let futures = all_data.slice(i + 1, i + 61);
+                      if (futures.length == 60) {
+                        var buy = undefined;
+                        var sell = undefined;
+                        [...futures].forEach((d) => {
+                          if (!buy && d.low < meta.band.lower && d.close > meta.band.lower) {
+                            buy = d.close
+                          }
+                          if (buy && d.high > buy * 1.05) {
+                            sell = d.high
+                          }
+                        })
+                        // var max_point = [...futures].sort((a, b) => b.high - a.high)[0];
+                        // var high_rate = max_point.high / row.close * 100;
+                        if (sell && buy) {
+                          row['result'] = sell / buy * 100;
+                        }
+
+                        if (buy && !sell) {
+                          row['result'] = "매도 실패"
+                        }
+
+                        if (!buy) {
+                          row['result'] = "매수 실패"
+                        }
+                      }
+
+                      // row['meta'] = JSON.stringify(meta);
+                      recommended_rows.push(row)
+                    }
+                  }
+                  prev_result = result;
+
                 } catch (error) {
                   console.log(error)
                 }
@@ -281,8 +250,8 @@ module.exports = {
       }
       res.status(200).send('OK');
     },
-    "suggest": async(req,res,next) => {
-      const query = req.query.date ? { date: new Date(req.query.date).getTime()-32400000, marker:'매수' } : { date: new Date(moment().format('YYYY-MM-DD')).getTime()-32400000, marker:'매수'};
+    "suggest": async (req, res, next) => {
+      const query = req.query.date ? { date: new Date(req.query.date).getTime() - 32400000, marker: '매수' } : { date: new Date(moment().format('YYYY-MM-DD')).getTime() - 32400000, marker: '매수' };
       const stockData = new connector.types.StockData(connector.database);
       const origin_data = await stockData.select(query)
 
@@ -291,7 +260,7 @@ module.exports = {
         return d
       }))
     },
-    "test": async(req,res,next) => {
+    "test": async (req, res, next) => {
       const stockData = new connector.types.StockData(connector.database);
       const origin_data = await stockData.select()
 
