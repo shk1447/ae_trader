0e482e91b4a70a109eb664e736a0a1184f36f99d
shk1447 <sukan8822@gmail.com>
2021-11-27 23:47:25 +0900

    자동거래를 위한 configure 추가

--- server/configure.js
+++ server/configure.js
@@ -16,6 +16,10 @@ process.env.google_id = config.google_auth.id;
 process.env.google_secret = config.google_auth.secret;
 process.env.google_domain = config.google_auth.domain;
 
+process.env.UPBIT_OPEN_API_ACCESS_KEY = config.upbit.access_key;
+process.env.UPBIT_OPEN_API_SECRET_KEY = config.upbit.secret_key;
+process.env.UPBIT_OPEN_API_SERVER_URL = config.upbit.server_url;
+
 process.env.smtp = config.smtp;
 
 module.exports = config;
--- server/routes/api/bitcoin/index.js
+++ server/routes/api/bitcoin/index.js
@@ -1,16 +1,77 @@
 const wait = require('waait')
 const axios = require('axios');
 const analysis = require('../../modules/Analysis');
+
+const request = require('request')
+const { v4: uuidv4 } = require("uuid")
+const crypto = require('crypto')
+const sign = require('jsonwebtoken').sign
+const queryEncode = require("querystring").encode
+
+const access_key = process.env.UPBIT_OPEN_API_ACCESS_KEY
+const secret_key = process.env.UPBIT_OPEN_API_SECRET_KEY
+const server_url = process.env.UPBIT_OPEN_API_SERVER_URL
+
 // const { BollingerBands } = require('technicalindicators');
 // 비트코인도 가보즈아~~~!!
 module.exports = {
   get: {
+    "auto_trading": async (req, res, next) => {
+      const payload = {
+        access_key: access_key,
+        nonce: uuidv4(),
+      }
+
+      const token = sign(payload, secret_key)
+
+      request({
+        method: "GET",
+        url: server_url + "/v1/accounts",
+        headers: { Authorization: `Bearer ${token}` },
+      }, (error, response, body) => {
+        if (error) throw new Error(error)
+        console.log(body)
+      })
+
+      // const body = {
+      //   market: 'KRW-BTC',
+      //   side: 'bid', // ask
+      //   volume: '0.01',
+      //   price: '100',
+      //   ord_type: 'limit',
+      // }
+
+      // const query = queryEncode(body)
+
+      // const hash = crypto.createHash('sha512')
+      // const queryHash = hash.update(query, 'utf-8').digest('hex')
+
+      // const payload = {
+      //   access_key: access_key,
+      //   nonce: uuidv4(),
+      //   query_hash: queryHash,
+      //   query_hash_alg: 'SHA512',
+      // }
+
+      // const token = sign(payload, secret_key)
+
+      // request({
+      //   method: "POST",
+      //   url: server_url + "/v1/orders",
+      //   headers: { Authorization: `Bearer ${token}` },
+      //   json: body
+      // }, (error, response, body) => {
+      //   if (error) throw new Error(error)
+      //   console.log(body)
+      // })
+
+    },
     "analysis": async (req, res, next) => {
-      const response = await axios.get('https://api.upbit.com/v1/market/all');
+      const response = await axios.get(`${server_url}/v1/market/all`);
       const list = response.data.filter((d) => d.market.includes('KRW-'))
 
       const getInsight = async (minutes, market) => {
-        const { data } = await axios.get(`https://api.upbit.com/v1/candles/minutes/${minutes}?market=${market}&count=200`)
+        const { data } = await axios.get(`${server_url}/v1/candles/minutes/${minutes}?market=${market}&count=200`)
         data.sort((a, b) => a.timestamp - b.timestamp)
         const rows = data.map((d) => {
           return {
@@ -53,7 +114,7 @@ module.exports = {
           const buy_price = (long.insight.support_price + long.row.high) / 2;
 
           if (long.row.low < buy_price && long.row.close > buy_price) {
-            console.log(list[i].market, buy_price, long.row.low);
+            console.log(list[i].market, buy_price, (curr.insight.support + curr.insight.future_resist) > (curr.insight.future_support + curr.insight.resist));
           }
 
         }
