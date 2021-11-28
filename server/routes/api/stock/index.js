a5401d3787d99330398c43d7235960c3063a7047
shk1447 <sukan8822@gmail.com>
2021-11-28 14:13:09 +0900

    buy price 추가 및 status시 수집

--- server/routes/api/stock/index.js
+++ server/routes/api/stock/index.js
@@ -10,6 +10,150 @@ const { segmentation } = require('../../modules/Analysis');
 const { IchimokuCloud, BollingerBands, OBV, SMA } = require('technicalindicators');
 
 let collecting = false;
+
+const collectFunc = async (code, days) => {
+  const stockList = await connector.dao.StockList.select(code);
+
+  const progress_bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+  progress_bar.start(stockList.length, 0);
+
+  const nextStep = async (step) => {
+    if (step < stockList.length) {
+      var item = stockList[step];
+      connector.dao.StockData.table_name = "stock_data_" + item.stock_code;
+
+      let rows = [];
+      let recommended_rows = []
+      const data = await collector.getSise(item.stock_code, days);
+      if (data.length > 0) {
+        const origin_data = await connector.dao.StockData.getTable().where('date', '<', data[0].date);
+        const all_data = origin_data.concat(data);
+
+        let prev_result;
+        for (let i = all_data.length - data.length; i < all_data.length; i++) {
+          let row = all_data[i];
+          row['code'] = item.stock_code;
+          try {
+            var day_power = (row.close - row.open);
+            var down_power = (row.high - Math.min(row.open, row.close));
+            var up_power = (Math.max(row.open, row.close) - row.low);
+            var power = (up_power - down_power + day_power) * (row.volume / item.stock_total);
+
+            let result = {
+              curr_trend: 0,
+              init_trend: 0,
+              segmentation: [],
+              upward_point: [],
+              downward_point: [],
+            };
+
+            analysis.segmentation([...all_data].splice(0, i + 1), result, 'close');
+            let insight = analysis.cross_point(result, row, 'close');
+
+            result.segmentation.sort((a, b) => a.from.date - b.from.date)
+            result.upward_point.sort((a, b) => a.date - b.date)
+            result.downward_point.sort((a, b) => a.date - b.date)
+
+            let meta = {
+              curr_trend: result.curr_trend,
+              init_trend: result.init_trend,
+              segmentation: result.segmentation.length,
+              upward_point: result.upward_point.length,
+              downward_point: result.downward_point.length,
+              insight: insight,
+              power: power,
+              date: moment(row.date).format('YYYY-MM-DD')
+            };
+
+            let clouds = new IchimokuCloud({
+              high: [...all_data].slice(i - 77, i).map((d) => d.high),
+              low: [...all_data].slice(i - 77, i).map((d) => d.low),
+              conversionPeriod: 9,
+              basePeriod: 26,
+              spanPeriod: 52,
+              displacement: 26
+            }).result;
+
+            let bands = new BollingerBands({
+              period: 20,
+              stdDev: 2,
+              values: [...all_data].slice(i - 20, i).map((d) => d.close),
+            }).result
+            if (bands.length > 0) {
+              let band_data = bands;
+              meta['band'] = band_data[0];
+            }
+
+            if (clouds.length > 0) {
+              let cloud_data = clouds;
+              meta['cloud'] = {
+                spanA: clouds[0].spanA,
+                spanB: clouds[0].spanB,
+                conversion: clouds[clouds.length - 1].conversion,
+                base: clouds[clouds.length - 1].base,
+              };
+            }
+            /*
+              spanA > spanB 양운
+              spanA < spanB 음운
+              conversion 전환선
+              base 기준선
+            */
+
+            result['meta'] = meta;
+
+            if (prev_result) {
+              if (!prev_result.meta.insight.support_price && insight.support_price && (insight.support + insight.future_resist) > (insight.future_support + insight.resist)) {
+                row['marker'] = '매수';
+                let futures = all_data.slice(i + 1, i + 61);
+                if (futures.length == 60) {
+                  var max_point = [...futures].sort((a, b) => b.high - a.high)[0];
+                  var high_rate = max_point.high / row.close * 100;
+
+                  row['result'] = high_rate
+
+                }
+                recommended_rows.push(row)
+              }
+            }
+
+            row['meta'] = JSON.stringify(meta);
+            prev_result = result;
+
+          } catch (error) {
+            console.log(error)
+          }
+
+          rows.push(row);
+        }
+
+        if (rows.length > 0) {
+          if (rows.length > 50) {
+            await connector.dao.StockData.batchInsert(rows);
+          } else {
+            await connector.dao.StockData.insert(rows).onConflict(['code', 'date']).merge();
+          }
+        }
+
+        if (recommended_rows.length > 0) {
+          connector.dao.StockData.table_name = "stock_data";
+          if (recommended_rows.length > 50) {
+            await connector.dao.StockData.batchInsert(recommended_rows);
+          } else {
+            await connector.dao.StockData.insert(recommended_rows).onConflict(['code', 'date']).merge();
+          }
+        }
+      }
+
+      progress_bar.update(step + 1);
+      nextStep(step + 1);
+    } else {
+      collecting = false;
+      progress_bar.stop();
+    }
+  }
+  nextStep(0);
+}
 module.exports = {
   get: {
     "initialize": async (req, res, next) => {
@@ -74,147 +218,8 @@ module.exports = {
         const collect_job_func = async () => {
           const code = req.query.code ? { stock_code: req.query.code } : {};
           const days = req.query.days ? parseInt(req.query.days) : 5;
-          const stockList = await connector.dao.StockList.select(code);
 
-          const progress_bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-          progress_bar.start(stockList.length, 0);
-
-          const nextStep = async (step) => {
-            if (step < stockList.length) {
-              var item = stockList[step];
-              connector.dao.StockData.table_name = "stock_data_" + item.stock_code;
-
-              let rows = [];
-              let recommended_rows = []
-              const data = await collector.getSise(item.stock_code, days);
-              if (data.length > 0) {
-                const origin_data = await connector.dao.StockData.getTable().where('date', '<', data[0].date);
-                const all_data = origin_data.concat(data);
-
-                let prev_result;
-                for (let i = all_data.length - data.length; i < all_data.length; i++) {
-                  let row = all_data[i];
-                  row['code'] = item.stock_code;
-                  try {
-                    var day_power = (row.close - row.open);
-                    var down_power = (row.high - Math.min(row.open, row.close));
-                    var up_power = (Math.max(row.open, row.close) - row.low);
-                    var power = (up_power - down_power + day_power) * (row.volume / item.stock_total);
-
-                    let result = {
-                      curr_trend: 0,
-                      init_trend: 0,
-                      segmentation: [],
-                      upward_point: [],
-                      downward_point: [],
-                    };
-
-                    analysis.segmentation([...all_data].splice(0, i + 1), result, 'close');
-                    let insight = analysis.cross_point(result, row, 'close');
-
-                    result.segmentation.sort((a, b) => a.from.date - b.from.date)
-                    result.upward_point.sort((a, b) => a.date - b.date)
-                    result.downward_point.sort((a, b) => a.date - b.date)
-
-                    let meta = {
-                      curr_trend: result.curr_trend,
-                      init_trend: result.init_trend,
-                      segmentation: result.segmentation.length,
-                      upward_point: result.upward_point.length,
-                      downward_point: result.downward_point.length,
-                      insight: insight,
-                      power: power,
-                      date: moment(row.date).format('YYYY-MM-DD')
-                    };
-
-                    let clouds = new IchimokuCloud({
-                      high: [...all_data].slice(i - 77, i).map((d) => d.high),
-                      low: [...all_data].slice(i - 77, i).map((d) => d.low),
-                      conversionPeriod: 9,
-                      basePeriod: 26,
-                      spanPeriod: 52,
-                      displacement: 26
-                    }).result;
-
-                    let bands = new BollingerBands({
-                      period: 20,
-                      stdDev: 2,
-                      values: [...all_data].slice(i - 20, i).map((d) => d.close),
-                    }).result
-                    if (bands.length > 0) {
-                      let band_data = bands;
-                      meta['band'] = band_data[0];
-                    }
-
-                    if (clouds.length > 0) {
-                      let cloud_data = clouds;
-                      meta['cloud'] = {
-                        spanA: clouds[0].spanA,
-                        spanB: clouds[0].spanB,
-                        conversion: clouds[clouds.length - 1].conversion,
-                        base: clouds[clouds.length - 1].base,
-                      };
-                    }
-                    /*
-                      spanA > spanB 양운
-                      spanA < spanB 음운
-                      conversion 전환선
-                      base 기준선
-                    */
-
-                    result['meta'] = meta;
-
-                    if (prev_result) {
-                      if (!prev_result.meta.insight.support_price && insight.support_price && (insight.support + insight.future_resist) > (insight.future_support + insight.resist)) {
-                        row['marker'] = '매수';
-                        let futures = all_data.slice(i + 1, i + 61);
-                        if (futures.length == 60) {
-                          var max_point = [...futures].sort((a, b) => b.high - a.high)[0];
-                          var high_rate = max_point.high / row.close * 100;
-
-                          row['result'] = high_rate
-
-                        }
-                        recommended_rows.push(row)
-                      }
-                    }
-
-                    row['meta'] = JSON.stringify(meta);
-                    prev_result = result;
-
-                  } catch (error) {
-                    console.log(error)
-                  }
-
-                  rows.push(row);
-                }
-
-                if (rows.length > 0) {
-                  if (rows.length > 50) {
-                    await connector.dao.StockData.batchInsert(rows);
-                  } else {
-                    await connector.dao.StockData.insert(rows).onConflict(['code', 'date']).merge();
-                  }
-                }
-
-                if (recommended_rows.length > 0) {
-                  connector.dao.StockData.table_name = "stock_data";
-                  if (recommended_rows.length > 50) {
-                    await connector.dao.StockData.batchInsert(recommended_rows);
-                  } else {
-                    await connector.dao.StockData.insert(recommended_rows).onConflict(['code', 'date']).merge();
-                  }
-                }
-              }
-
-              progress_bar.update(step + 1);
-              nextStep(step + 1);
-            } else {
-              collecting = false;
-              progress_bar.stop();
-            }
-          }
-          nextStep(0);
+          collectFunc(code, days);
         }
 
         if (req.query.cron) {
@@ -319,6 +324,9 @@ module.exports = {
       // 최종 매도에 대한 결정 및 가격 제공 ( 테스트 필요함. )
       const code = req.query.code;
       const req_date = req.query.date ? new Date(req.query.date).getTime() - 32400000 : new Date(moment().format('YYYY-MM-DD')).getTime() - 32400000;
+
+      await collectFunc({ stock_code: code }, 1);
+
       const stockData = new connector.types.StockData(connector.database);
       stockData.table_name = "stock_data_" + code;
 
