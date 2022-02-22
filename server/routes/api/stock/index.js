59a53f385849bef2e36aeaaa07d585d6dd9f6d1e
seokhwan.kim <sukan8822@gmail.com>
2022-02-22 20:47:23 +0900

    change cron

--- server/routes/api/stock/index.js
+++ server/routes/api/stock/index.js
@@ -259,7 +259,7 @@ if (cluster.isMaster) {
   console.log("master!!!");
   var CronJob = require("cron").CronJob;
   var collect_job = new CronJob(
-    "50 8,15 * * 1-5",
+    "50 8,12,15 * * 1-5",
     collect_job_func,
     null,
     false,
